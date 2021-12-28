2c6c11cc75f117a35ce31d8ab58fd452e32a3e2c
Scott Rosenberg <48493016+vrabbi@users.noreply.github.com>
2021-12-28 21:28:36 +0200

    Create 03-path-code-server-config.sh

---
+++ workshop/setup.d/03-path-code-server-config.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+cat <<EOF > /opt/eduk8s/sbin/start-code-server
+#!/bin/bash
+
+set -x
+
+set -eo pipefail
+
+CODE_SERVER_BIND_ADDRESS=${CODE_SERVER_BIND_ADDRESS:-127.0.0.1}
+
+EDITOR_HOME=${EDITOR_HOME:-/home/eduk8s/java-web-app}
+
+exec /opt/code-server/bin/code-server \
+    --bind-addr "$CODE_SERVER_BIND_ADDRESS:10085" \
+    --auth none \
+    --disable-telemetry \
+    $EDITOR_HOME
+EOF
+/opt/eduk8s/sbin/start-code-server &
