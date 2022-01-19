4414f67e99b516dbb974c3ce2a3ab9f13901433d
Scott Rosenberg <48493016+vrabbi@users.noreply.github.com>
2022-01-19 18:37:17 +0200

    Update 02-dev-namespace.sh

--- workshop/setup.d/02-dev-namespace.sh
+++ workshop/setup.d/02-dev-namespace.sh
@@ -3,5 +3,5 @@ set -x
 set +e
 
 REGISTRY_PASSWORD=$HARBOR_PASSWORD kp secret create registry-credentials --registry harbor.${INGRESS_DOMAIN} --registry-user admin
-
+kubectl patch serviceaccount default --type='json' -p='[{"op":"add","path":"/imagePullSecrets/-", "value":{"name":"tap-registry"}}]'
 docker login harbor.${INGRESS_DOMAIN} -u admin -p $HARBOR_PASSWORD
